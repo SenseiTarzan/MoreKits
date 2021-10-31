18185600d73db3a7889ca91f42dad9d31cec6880
Gaëtan H <steellgold0@gmail.com>
2021-10-31 14:06:21 +0100

    Add default class

--- src/Steellgold/MoreKits/MK.php
+++ src/Steellgold/MoreKits/MK.php
@@ -10,3 +10,25 @@ namespace Steellgold\MoreKits;
 use pocketmine\plugin\PluginBase;
 
 class MK extends PluginBase {
+    private string $version = "1.0";
+    public static MK $instance;
+
+    public function onEnable() {
+        self::setInstance($this);
+
+        if ($this->getConfig()->exists("version") and $this->getConfig()->get("version") !== $this->version) {
+            $this->getLogger()->alert("The plug-in configuration has been modified since an update, your old configuration has been renamed to old_config.yml");
+            rename($this->getDataFolder() . "config.yml", $this->getDataFolder() . "old_config.yml");
+            $this->getServer()->getPluginManager()->disablePlugin($this);
+            return;
+        }
+    }
+
+    public static function getInstance() : MK {
+        return self::$instance;
+    }
+
+    public static function setInstance($instance): void {
+        self::$instance = $instance;
+    }
+}
