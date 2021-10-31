e6f18a89742b921b77d428efa65ff316d5beacd2
Gaëtan H <steellgold0@gmail.com>
2021-10-31 14:41:29 +0100

    Add Commando

--- src/Steellgold/MoreKits/MK.php
+++ src/Steellgold/MoreKits/MK.php
@@ -7,12 +7,17 @@
 
 namespace Steellgold\MoreKits;
 
+use CortexPE\Commando\exception\HookAlreadyRegistered;
+use CortexPE\Commando\PacketHooker;
 use pocketmine\plugin\PluginBase;
 
 class MK extends PluginBase {
     private string $version = "1.0";
     public static MK $instance;
 
+    /**
+     * @throws HookAlreadyRegistered
+     */
     public function onEnable() {
         self::setInstance($this);
 
@@ -21,6 +26,10 @@ class MK extends PluginBase {
             rename($this->getDataFolder() . "config.yml", $this->getDataFolder() . "old_config.yml");
             $this->getServer()->getPluginManager()->disablePlugin($this);
             return;
+        }
+
+        if(!PacketHooker::isRegistered()) {
+            PacketHooker::register($this);
         }
     }
 
